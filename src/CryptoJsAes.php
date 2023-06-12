f6fd952238c90b250e1696527b48a3e89af42838
BrainFooLong <brainfoolong@users.noreply.github.com>
2023-06-12 08:04:37 +0200

    reverted prev merges as they break functionality

--- src/CryptoJsAes.php
+++ src/CryptoJsAes.php
@@ -2,6 +2,8 @@
 
 namespace Nullix\CryptoJsAes;
 
+use const OPENSSL_RAW_DATA;
+
 /**
  * Encrypt/Decrypt data from Javascript's CryptoJS
  * PHP 7.x and later supported
@@ -20,12 +22,14 @@ class CryptoJsAes
     public static function encrypt($value, string $passphrase)
     {
         $salt = openssl_random_pseudo_bytes(8);
-        $iv = openssl_random_pseudo_bytes(16);
-        
-        $concatedPassphrase = $passphrase . $salt;
-        $key = md5($concatedPassphrase, true);
-        $key .= md5($key . $concatedPassphrase, true);
-
+        $salted = '';
+        $dx = '';
+        while (strlen($salted) < 48) {
+            $dx = md5($dx . $passphrase . $salt, true);
+            $salted .= $dx;
+        }
+        $key = substr($salted, 0, 32);
+        $iv = substr($salted, 32, 16);
         $encrypted_data = openssl_encrypt(json_encode($value), 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
         $data = ["ct" => base64_encode($encrypted_data), "iv" => bin2hex($iv), "s" => bin2hex($salt)];
         return json_encode($data);
@@ -44,8 +48,14 @@ class CryptoJsAes
         $iv = hex2bin($json["iv"]);
         $ct = base64_decode($json["ct"]);
         $concatedPassphrase = $passphrase . $salt;
-        $key = md5($concatedPassphrase, true);
-        $key .= md5($key . $concatedPassphrase, true);
+        $md5 = [];
+        $md5[0] = md5($concatedPassphrase, true);
+        $result = $md5[0];
+        for ($i = 1; $i < 3; $i++) {
+            $md5[$i] = md5($md5[$i - 1] . $concatedPassphrase, true);
+            $result .= $md5[$i];
+        }
+        $key = substr($result, 0, 32);
         $data = openssl_decrypt($ct, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
         return json_decode($data, true);
     }
