9cca6af090446263cade6f42b5ad9d0b77bf98ee
Roland Eigelsreiter <brainfoolong@users.noreply.github.com>
2023-12-29 17:48:09 +0100

    fixed potential xss in example scripts

--- dist/example-php.php
+++ dist/example-php.php
@@ -8,7 +8,7 @@ require __DIR__ . "/../src/CryptoJsAes.php";
 $originalValue = ["Coming from PHP - We do encrypt an array", "123", ['nested']]; // this could be any value
 $password = "123456";
 $encrypted = CryptoJsAes::encrypt($originalValue, $password);
-echo "Encrypted:<br/><a href='example-js.html?encrypted=" . base64_encode($encrypted) . "' title='Pass to JS testpage' target='_blank'>" . $encrypted . "</a><br/><br/>\n";
+echo "Encrypted:<br/><a href='example-js.html?encrypted=" . base64_encode($encrypted) . "' title='Pass to JS testpage' target='_blank'>" . htmlentities($encrypted) . "</a><br/><br/>\n";
 // something like: {"ct":"g9uYq0DJypTfiyQAspfUCkf+\/tpoW4DrZrpw0Tngrv10r+\/yeJMeseBwDtJ5gTnx","iv":"c8fdc314b9d9acad7bea9a865671ea51","s":"7e61a4cd341279af"}
 
 // decrypt
@@ -16,4 +16,4 @@ $encrypted = isset($_GET['encrypted']) ? base64_decode($_GET['encrypted']) : $en
 $password = "123456";
 $decrypted = CryptoJsAes::decrypt($encrypted, $password);
 
-echo "Decrypted (From $encrypted):<br/>" . print_r($decrypted, true);
+echo "Decrypted (From " . htmlentities($encrypted) . "):<br/>" . htmlentities(print_r($decrypted, true));
